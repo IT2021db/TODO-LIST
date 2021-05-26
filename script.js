16b5f0f3dba5d3dc5680e1081653320a66a093e0
Dorota Borowska <youcode2021db@gmail.com>
2021-05-26 20:39:46 +0200

    remove task

--- script.js
+++ script.js
@@ -19,23 +19,49 @@
         render();
     }
 
-   
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    };
+    const removeTask=(taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
         for (const task of tasks) {
             htmlString += `
-        <li><div class="div">
-        ${task.content} 
-        </li></div>
+        <li
+        ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+        <button class="js-done">done</button>
+        ${task.content}
+        <button class="js-remove">usuń</button>
+        
+        </li>
         `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+        });
+       
     };
     const init = () => {
         render();
-        
-      
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+
+
     };
     init();
 }
